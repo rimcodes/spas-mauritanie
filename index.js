a5f4dc9753bbbe0308e19d1b234a119c7d84018c
unknown <med4en@gmail.com>
2022-10-27 20:46:38 +0000

    jwt auth

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.options('*', cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(morgan('tiny'));
-// app.use(authJwt());
+app.use(authJwt());
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'))
 // Response to the backend calls
 // app.use('/', express.static(__dirname + '/public/app'))
